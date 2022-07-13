e006186ae8b5a0b4efccc3da0690f25402aea0dd
Ameer Jamal <ameer10194@gmail.comm>
2022-07-13 15:44:27 +0300

    Tried out two way binding and used data binding plus property binding, alone with adding functions to bind this code with

--- my-new-app/src/app/servers/servers.component.ts
+++ my-new-app/src/app/servers/servers.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus="No server has been created!";
+  initialServerName='Server Name Here';
+  serverName= this.initialServerName;
+  constructor() { 
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
 
+
+  onCreateServer(){
+    this.serverCreationStatus="The server by the name: \" "+this.serverName+" \" has been created";
+  }
+  onResetServerName(){
+    this.serverName= '';
+    this.allowNewServer=false;
+  }
+
+  onUpdateServerName(e: any ){
+    if(this.serverName ==  this.initialServerName || !this.serverName){
+      this.allowNewServer=false;
+     }
+     else{
+      this.allowNewServer=true;
+      this.serverName = (<HTMLInputElement>e.target).value
+    }
+  }
 }

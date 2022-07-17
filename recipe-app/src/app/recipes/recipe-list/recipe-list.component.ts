a29cf92d4ca4cad7095a5106a7e38a8953dd6657
Ameer Jamal <ameer10194@gmail.comm>
2022-07-17 09:57:48 +0300

    -Made Header Component
    -Made Class(Modal) Of Recipie
    
    -Added List of recipies and detail
    
    -of recipies into tha main recipe compononet
    
    -Implmented the recipe list component which involves displaying name,description and image of recipe
    
    -constructed two recipie objects into the recipe ts
    
    -Created a genral outline for the shopping list

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes:Recipe[] =[
+  new Recipe("Grilled Cheese","1-put cheese in bread 2-grill until cheese melts","https://assets.bonappetit.com/photos/57acf62a53e63daf11a4dbee/5:7/w_1244,h_1742,c_limit/best-ever-grilled-cheese.jpg"),
+  new Recipe("Chicken Parm","1-Make Chicken 2-Parmify","https://assets.bonappetit.com/photos/5ea8f0df16738800085ad5d2/1:1/w_2560%2Cc_limit/Chicken-Parmesean-Recipe-Lede.jpg"),
 
-  constructor() { }
+]
+  
+constructor() { }
 
   ngOnInit(): void {
   }

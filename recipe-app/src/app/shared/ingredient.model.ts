ca554662bbb5a36ec97b73bd0866f81307ceda13
Ameer Jamal <ameer10194@gmail.comm>
2022-07-21 16:06:11 +0300

    -Made Recipie detail - Made A Model for Ingredients -Add Shopping list that shows the ingredients you need -added a couple of ingredients to dispaly -Made shopping edit component

---
+++ recipe-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+export class Ingredient{
+    constructor(public name:String, public amount:Number){}
+}
+
+// On top is the same as below 
+//
+// export class Ingredient{
+//     public name: String;
+//     public amount: Number;
+    
+//     constructor(name:String , amount:Number) {
+//         this.name = name;
+//         this.amount = amount;
+//     }
+// }
+
